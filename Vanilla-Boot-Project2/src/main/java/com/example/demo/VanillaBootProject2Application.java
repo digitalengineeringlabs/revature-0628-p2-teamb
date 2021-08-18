963db298565e3da8ed833cd424d574ae609ae8b6
Aivansky <51238426+Aivansky@users.noreply.github.com>
2021-08-18 13:18:27 -0700

    Add files via upload

---
+++ Vanilla-Boot-Project2/src/main/java/com/example/demo/VanillaBootProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaBootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanillaBootProject2Application.class, args);
+	}
+
+}
