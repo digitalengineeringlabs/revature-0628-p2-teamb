756917a4d027838157fb2aebb155557ba72249c4
Aivansky <orenlascari@gmail.com>
2021-08-26 13:56:54 -0700

    adding approve feature not done yet

--- Vanilla-Boot-Project2/src/main/java/com/example/demo/controller/TicketController.java
+++ Vanilla-Boot-Project2/src/main/java/com/example/demo/controller/TicketController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Ticket;
+import com.example.demo.repository.Repository;
 import com.example.demo.services.TicketService;
+
+
+
+
 
 @RestController
 @CrossOrigin
@@ -18,11 +30,30 @@ public class TicketController {
 
 	@Autowired
 	private TicketService service;
+	Repository repository;
 	
 	@GetMapping("/Alltickets")
 	public List<Ticket> getTickets(){
 		return service.getTickets();
 	}
 
+	@PutMapping("/tutorials/{id}")
+	public ResponseEntity<Ticket> updateTutorial(@PathVariable("id") long id, @RequestBody Ticket riembursement) {
+		Optional<Ticket> tutorialData = repository.findById(id);
+
+		if (tutorialData.isPresent()) {
+			Ticket _tutorial = tutorialData.get();
+			_tutorial.setAmount(riembursement.getAmount());
+			_tutorial.setDescription(riembursement.getDescription());
+			_tutorial.setType(riembursement.getType());
+			_tutorial.setId(riembursement.getId());
+			_tutorial.setStatus(riembursement.getStatus());
+			
+			return new ResponseEntity<>(repository.save(_tutorial), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
 	
 }
--- client/src/app/ticket/ticket.component.ts
+++ client/src/app/ticket/ticket.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8080/tutorials';
 
 export class Ticket {
   constructor(
-    public id: number,
-    public amount: number,
-    public description: string,
-    public status: string,
-    public time: Date,
-    public type: string,
-    public employee_id: number
+    public id?: number,
+    public amount?: number,
+    public description?: string,
+    public status?: string,
+    public time?: Date,
+    public type?: string,
+    public employee_id?: number
   ) {
   }
 }
@@ -23,9 +26,12 @@ export class Ticket {
 export class TicketComponent implements OnInit {
 
   tickets: Ticket[] | undefined;
+  currentTutorial: Ticket = {};
+
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -41,4 +47,28 @@ export class TicketComponent implements OnInit {
     );
   }
 
+  update(id: any, data: any): Observable<any> {
+    return this.httpClient.put(`${baseUrl}/${id}`, data);
+  }
+
+  updateApproved(status2: any){
+    const data = {
+      description: this.currentTutorial.description,
+      status: status2='approved' 
+    };
+    console.log(data);
+    this.update(this.currentTutorial.id, data)
+    .subscribe(
+      response => {
+          this.currentTutorial.status = 'approved';
+          console.log(response);
+       
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
 }
+
+
