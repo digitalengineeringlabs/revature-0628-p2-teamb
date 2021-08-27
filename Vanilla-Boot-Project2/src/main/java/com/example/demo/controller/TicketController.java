ab147f1114c9992d2ed6dbc9c3a8c2a463d65e2f
alec-patterson <alec.patterson@revature.net>
2021-08-26 21:18:15 -0700

    final little fixes

--- Vanilla-Boot-Project2/src/main/java/com/example/demo/controller/TicketController.java
+++ Vanilla-Boot-Project2/src/main/java/com/example/demo/controller/TicketController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
